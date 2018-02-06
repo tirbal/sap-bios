db8ae7388c074a895b88655782dbebd0a3daad84
Louis d'Hérouville <louis.dherouville@bioserenity.com>
2018-02-06 22:19:30 +0100

    Add AddRowService

---
+++ src/main/java/services/AddRowService.java
@@ -0,0 +1,30 @@
+package services;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.By.ByTagName;
+import org.openqa.selenium.By.ByXPath;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class AddRowService extends BaseService {
+
+    WebElement addButton;
+
+    public AddRowService(WebDriver driver) {
+        super(driver);
+
+    }
+
+    private void addRow(String task, String activity) {
+        this.driver.findElement(new ByXPath("//*[contains(text(), 'Add Row')]")).click();
+
+    }
+
+
+    @Override
+    public boolean launch() {
+
+        this.addRow("","");
+        return true;
+    }
+}
